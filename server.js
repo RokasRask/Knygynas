e230a35bc765807b4f5e08d33587fa1a8996d86c
RokRask <rokas.raskevic@gmail.com>
2024-12-05 13:34:03 +0200

    commit

--- server.js
+++ server.js
@@ -15,6 +15,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const top = fs.readFileSync('./html/top.html', 'utf8');
 const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
 
+// MESSAGES
+
+const messages = {
+    create_success: { msg: 'Knyga sėkmingai sukurta!', type: 'success' },
+    edit_success: { msg: 'Knyga sėkmingai atnaujinta!', type: 'success' },
+
+};
+
+const getMessages = msg => {
+    if (!msg) return null;
+    const message = messages[msg];
+    if (!message) return null;
+    return message;
+}
+
 // ROUTES
 
 app.get('/', (req, res) => {
@@ -27,8 +42,9 @@ app.get('/', (req, res) => {
     const data = {
         pageTitle: 'Knygų sąrašas',
         domain: domain,
-        books
-     };
+        books,
+        message: getMessages(req.query.msg)
+    };
     const html = template(data);
     res.send(html);
 });
@@ -41,7 +57,34 @@ app.get('/create', (req, res) => {
         manoKintamasis: 'Labas',
         pageTitle: 'Nauja knyga',
         domain: domain
-     };
+    };
+    const html = template(data);
+    res.send(html);
+});
+
+app.get('/edit/:id', (req, res) => {
+
+    const file = top + fs.readFileSync('./html/edit.html', 'utf8') + bottom;
+    const template = handlebars.compile(file);
+
+    let books = fs.readFileSync('./data/books.json', 'utf8');
+    books = JSON.parse(books);
+    const id = req.params.id;
+
+    const book = books.find(book => book.id === id);
+
+    // validation
+
+    if (!book) {
+        res.status(404).send('Tokios knygos nėra');
+        return;
+    }
+
+    const data = {
+        pageTitle: `Redaguoti knygą ${book.title}`,
+        domain: domain,
+        ...book
+    };
     const html = template(data);
     res.send(html);
 });
@@ -60,7 +103,36 @@ app.post('/store', (req, res) => {
     data = JSON.stringify(data);
     fs.writeFileSync('./data/books.json', data);
 
-    res.status(302).redirect(domain);
+    res.status(302).redirect(domain + '?msg=create_success');
+
+
+});
+
+app.post('/update/:id', (req, res) => {
+
+    const { title, author, year, genre, isbn, pages } = req.body;
+
+    let books = fs.readFileSync('./data/books.json', 'utf8');
+    books = JSON.parse(books);
+    const id = req.params.id;
+
+    const oldBook = books.find(book => book.id === id);
+
+    // validation
+    if (!oldBook) {
+        res.status(404).send('Tokios knygos nėra');
+        return;
+    }
+
+    const newBook = { id: oldBook.id, title, author, year, genre, isbn, pages };
+
+    books = books.map(book => book.id === id ? newBook : book);
+
+
+    books = JSON.stringify(books);
+    fs.writeFileSync('./data/books.json', books);
+
+    res.status(302).redirect(domain + '?msg=edit_success');
 
 
 });
