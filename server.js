0b7cd9e00864edec52d5f80eaa31b57d65b3939b
RokRask <rokas.raskevic@gmail.com>
2024-12-12 11:12:08 +0200

    commit

--- server.js
+++ server.js
@@ -5,30 +5,62 @@ const handlebars = require('handlebars');
 const fs = require('node:fs');
 const { v4: uuidv4 } = require('uuid');
 const app = express();
-const port = 80;
 
 handlebars.registerHelper('isdefined', function (value) {
   return value !== undefined;
 });
 
+const port = 80;
 const domain = 'http://books.final/';
-
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-
 const top = fs.readFileSync('./html/top.html', 'utf8');
 const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
-
-// MESSAGES
-
 const messages = {
   create_success: { msg: 'Knyga sėkmingai sukurta!', type: 'success' },
   edit_success: { msg: 'Knyga sėkmingai atnaujinta!', type: 'success' },
   delete_success: { msg: 'Knyga sėkmingai ištrinta!', type: 'success' },
   validation_error: { msg: 'Užpildykite visus laukus!', type: 'danger' }
 };
+
+// MIDDLEWARE
+
+const sessionManager = (req, res, next) => {
+  let sessionId = req.cookies.session || '';
+  let sessionData = fs.readFileSync('./data/session.json', 'utf8');
+  sessionData = JSON.parse(sessionData);
+  const findSession = sessionData.find(s => s.id === sessionId);
+  if (sessionId && findSession) {
+    req.session = structuredClone(findSession);
+  } else {
+    sessionId = uuidv4();
+    const session = { id: sessionId, data: {} };
+    req.session = session;
+    sessionData.push(session);
+    sessionData = JSON.stringify(sessionData);
+    fs.writeFileSync('./data/session.json', sessionData);
+  }
+  res.cookie('session', sessionId, { maxAge: 1000 * 60 * 60 * 24 });
+  next();
+}
+
+const oldDataManager = (req, res, next) => {
+  const requestMethod = req.method;
+  if (requestMethod === 'GET') {
+    addToSession(req, 'oldData', {});
+  } else {
+    addToSession(req, 'oldData', req.body);
+  }
+  next(); 
+}
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(sessionManager);
+app.use(oldDataManager);
+
+
+// HELPERS
 
 const getMessages = req => {
   if (!req.session.data.msg) return null;
@@ -68,44 +100,9 @@ const show404 = res => {
   res.status(404).send(html);
 }
 
-// MIDDLEWARE
-
-const sessionManager = (req, res, next) => {
-  let sessionId = req.cookies.session || '';
-  let sessionData = fs.readFileSync('./data/session.json', 'utf8');
-  sessionData = JSON.parse(sessionData);
-  const findSession = sessionData.find(s => s.id === sessionId);
-  if (sessionId && findSession) {
-    req.session = structuredClone(findSession);
-  } else {
-    sessionId = uuidv4();
-    const session = { id: sessionId, data: {} };
-    req.session = session;
-    sessionData.push(session);
-    sessionData = JSON.stringify(sessionData);
-    fs.writeFileSync('./data/session.json', sessionData);
-  }
-  res.cookie('session', sessionId, { maxAge: 1000 * 60 * 60 * 24 });
-  next();
-}
-
-const oldDataManager = (req, res, next) => {
-  const requestMethod = req.method;
-  if (requestMethod === 'GET') {
-    addToSession(req, 'oldData', {});
-  } else {
-    addToSession(req, 'oldData', req.body);
-  }
-  next(); 
-}
-app.use(sessionManager);
-app.use(oldDataManager);
-
-
 // ROUTER
 
 app.get('/', (req, res) => {
-
   let books = fs.readFileSync('./data/books.json', 'utf8');
   books = JSON.parse(books);
   const file = top + fs.readFileSync('./html/read.html', 'utf8') + bottom;
@@ -119,7 +116,6 @@ app.get('/', (req, res) => {
   const html = template(data);
   res.send(html);
 });
-
 
 app.get('/create', (req, res) => {
   const file = top + fs.readFileSync('./html/create.html', 'utf8') + bottom;
@@ -135,22 +131,16 @@ app.get('/create', (req, res) => {
 });
 
 app.get('/edit/:id', (req, res) => {
-
   const file = top + fs.readFileSync('./html/edit.html', 'utf8') + bottom;
   const template = handlebars.compile(file);
-
   let books = fs.readFileSync('./data/books.json', 'utf8');
   books = JSON.parse(books);
   const id = req.params.id;
-
   const book = books.find(book => book.id === id);
-
-  // validation
   if (!book) {
     show404(res);
     return;
   }
-
   const data = {
     pageTitle: `Redaguoti knygą ${book.title}`,
     domain: domain,
@@ -160,26 +150,19 @@ app.get('/edit/:id', (req, res) => {
   };
   const html = template(data);
   res.send(html);
-
 });
 
 app.get('/show/:id', (req, res) => {
-
   const file = top + fs.readFileSync('./html/show.html', 'utf8') + bottom;
   const template = handlebars.compile(file);
-
   let books = fs.readFileSync('./data/books.json', 'utf8');
   books = JSON.parse(books);
   const id = req.params.id;
-
   const book = books.find(book => book.id === id);
-
-  // validation
   if (!book) {
-    res.status(404).send('Tokios knygos nėra');
+    show404(res);
     return;
   }
-
   const data = {
     pageTitle: `Rodyti knygą ${book.title}`,
     domain: domain,
@@ -187,26 +170,19 @@ app.get('/show/:id', (req, res) => {
   };
   const html = template(data);
   res.send(html);
-
 });
 
 app.get('/delete/:id', (req, res) => {
-
   const file = top + fs.readFileSync('./html/delete.html', 'utf8') + bottom;
   const template = handlebars.compile(file);
-
   let books = fs.readFileSync('./data/books.json', 'utf8');
   books = JSON.parse(books);
   const id = req.params.id;
-
   const book = books.find(book => book.id === id);
-
-  // validation
   if (!book) {
-    res.status(404).send('Tokios knygos nėra');
+    show404(res);
     return;
   }
-
   const data = {
     pageTitle: 'Trynimo patvirtinimas',
     domain: domain,
@@ -217,14 +193,9 @@ app.get('/delete/:id', (req, res) => {
   res.send(html);
 });
 
-
 app.post('/store', (req, res) => {
-
   const { title, author, year, genre, isbn, pages } = req.body;
   const id = uuidv4();
-
-  // need validation
-
   if (!title || !author || !year || !genre || !isbn || !pages) {
     addToSession(req, 'msg', 'validation_error');
     res.status(422).redirect(domain + 'create');
@@ -237,54 +208,39 @@ app.post('/store', (req, res) => {
   data = JSON.stringify(data);
   fs.writeFileSync('./data/books.json', data);
   addToSession(req, 'msg', 'create_success');
-  res.status(302).redirect(domain + '?msg=create_success');
+  res.status(302).redirect(domain);
 });
 
 app.post('/update/:id', (req, res) => {
-
   let books = fs.readFileSync('./data/books.json', 'utf8');
   books = JSON.parse(books);
   const id = req.params.id;
-
   const oldBook = books.find(book => book.id === id);
-
-  // page exists
   if (!oldBook) {
-    res.status(404).send('Tokios knygos nėra');
+    show404(res);
     return;
   }
-
   const { title, author, year, genre, isbn, pages } = req.body;
   if (!title || !author || !year || !genre || !isbn || !pages) {
     addToSession(req, 'msg', 'validation_error');
     res.status(422).redirect(domain + 'edit/' + id);
     return;
   }
-
   const newBook = { id: oldBook.id, title, author, year, genre, isbn, pages };
-
   books = books.map(book => book.id === id ? newBook : book);
-
   books = JSON.stringify(books);
   fs.writeFileSync('./data/books.json', books);
   addToSession(req, 'msg', 'edit_success');
   res.status(302).redirect(domain);
-
-
 });
 
-
 app.post('/destroy/:id', (req, res) => {
-
   let books = fs.readFileSync('./data/books.json', 'utf8');
   books = JSON.parse(books);
   const id = req.params.id;
-
   const oldBook = books.find(book => book.id === id);
-
-  // validation
   if (!oldBook) {
-    res.status(404).send('Tokios knygos nėra');
+    show404(res);
     return;
   }
   books = books.filter(book => book.id !== id);
@@ -293,8 +249,6 @@ app.post('/destroy/:id', (req, res) => {
   addToSession(req, 'msg', 'delete_success');
   res.status(302).redirect(domain);
 });
-
-
 
 app.listen(port, () => {
   console.log(`Knygynas darbui pasiruošęs ant ${port} porto!`);
